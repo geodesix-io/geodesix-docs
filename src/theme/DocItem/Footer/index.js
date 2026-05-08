474455b80b86628d5b316e62c69e8319a5130ef4
rachelrawraway <rachelrawraway@gmail.com>
2026-05-08 11:44:08 +0300

    Remove "Download as PDF"

--- src/theme/DocItem/Footer/index.js
+++ src/theme/DocItem/Footer/index.js
@@ -1,41 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import OriginalDocItemFooter from '@theme-original/DocItem/Footer';
 
 export default function DocItemFooter(props) {
-  const [loading, setLoading] = useState(false);
-
-  const handleDownload = async () => {
-    setLoading(true);
-    const html2pdf = (await import('html2pdf.js')).default;
-    const element = document.querySelector('.theme-doc-markdown');
-    const filename = document.title.replace(' | Geodesix Docs', '') + '.pdf';
-
-    await html2pdf()
-      .set({
-        margin: [15, 15, 15, 15],
-        filename,
-        image: { type: 'jpeg', quality: 0.98 },
-        html2canvas: { scale: 2, useCORS: true },
-        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
-      })
-      .from(element)
-      .save();
-
-    setLoading(false);
-  };
-
-  return (
-    <>
-      <OriginalDocItemFooter {...props} />
-      <div style={{ marginTop: '2rem', textAlign: 'right' }}>
-        <button
-          onClick={handleDownload}
-          disabled={loading}
-          className="button button--primary button--md"
-        >
-          {loading ? 'Generating PDF…' : 'Download as PDF'}
-        </button>
-      </div>
-    </>
-  );
+  return <OriginalDocItemFooter {...props} />;
 }
